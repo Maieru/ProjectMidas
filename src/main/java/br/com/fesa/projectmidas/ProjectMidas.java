5cae5c9fe95b5a019161943eb8386727eaaf9e68
081200022 <081200022@HPFS-CE-LAB6721.cefsa.corp.local>
2022-10-11 19:32:14 -0300

    Teste Daniel, alterando o código

--- src/main/java/br/com/fesa/projectmidas/ProjectMidas.java
+++ src/main/java/br/com/fesa/projectmidas/ProjectMidas.java
@@ -6,12 +6,15 @@ public class ProjectMidas {
         // Se você conseguiu ler essa mensagem, significa que tudo deu certo!
         // Adicione um comentário abaixo para testar o Push.
         //
-        //
+        // Daniel
         // João
         // Kauan
         //
         System.out.println("Eu tinha botado uns comentário aqui, é pra preencher ali encima!!!"); 
-       
+        System.out.println("De um sonho altruísta nasceu");
+        System.out.println("A semente se fez germinar");
+        System.out.println("Do ardoroso trabalho se ergueu");
+        System.out.println("Na nobre missão de ensinar");
         
     }
 }
