8e817c7bf8d7d0cd6b2912e08d9a485a27442d70
JOAO VICTOR MAIERU TEIXEIRA <081200025@faculdade.cefsa.edu.br>
2022-10-26 20:43:17 -0300

    Adição de tabela de transação

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,10 +1,20 @@
-CREATE SCHEMA MIDAS
+CREATE SCHEMA MIDAS;
 
-CREATE TABLE MIDAS.tbBanco (
+CREATE TABLE MIDAS.tbContaBanco (
     NumeroConta     BIGINT          NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
     Agencia         INTEGER         NOT NULL,
     Saldo           DOUBLE          NOT NULL,
-    NomeDoDono      VARCHAR(MAX)    NOT NULL,
-    CPFDoDono       VARCHAR(MAX)    NOT NULL
+    NomeDoDono      VARCHAR(200)    NOT NULL,
+    CPFDoDono       VARCHAR(15)     NOT NULL,
     PRIMARY KEY (Agencia, NumeroConta)
+);
+GO;
+
+CREATE TABLE MIDAS.tbTransferencia (
+    Codigo          BIGINT          NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    ContaOrigem     BIGINT          NOT NULL,
+    ContaDestino    BIGINT          NOT NULL,
+    DataDaTransacao DATE            NOT NULL,
+    Valor           DOUBLE          NOT NULL,
+    TipoTransacao   INTEGER         NOT NULL
 );
