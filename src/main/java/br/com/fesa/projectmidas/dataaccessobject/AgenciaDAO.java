0b2bfd50d77e57db045248581f81ff213075ccf3
Maieru <soljo@192.168.0.28>
2022-11-02 15:24:39 -0300

    Criação Agencia DAO

---
+++ src/main/java/br/com/fesa/projectmidas/dataaccessobject/AgenciaDAO.java
@@ -0,0 +1,160 @@
+package br.com.fesa.projectmidas.dataaccessobject;
+
+import br.com.fesa.projectmidas.exception.PersistenciaException;
+import br.com.fesa.projectmidas.model.Agencia;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AgenciaDAO implements GenericDAO<Agencia> {
+
+    private String nomeTabela = "MIDAS.TBTIPOINVESTIMENTO";
+
+    @Override
+    public List<Agencia> listar() throws PersistenciaException {
+        List<Agencia> agencias = new ArrayList();
+        String sql = String.format("SELECT * FROM %s", nomeTabela);
+        Connection connection = null;
+        try {
+            connection = Conexao.getInstance().getConnection();
+            PreparedStatement pStatement = connection.prepareStatement(sql);
+            ResultSet result = pStatement.executeQuery();
+            while (result.next()) {
+                agencias.add(montaObjeto(result));
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return agencias;
+    }
+
+    @Override
+    public void inserir(Agencia agencia) throws PersistenciaException {
+        String sql = String.format("INSERT INTO %s (NumeroAgencia, Localizacao) VALUES (?, ?)", nomeTabela);
+
+        Connection connection = null;
+        try {
+            connection = Conexao.getInstance().getConnection();
+            PreparedStatement pStatement = connection.prepareStatement(sql);
+
+            pStatement.setString(1, Integer.toString(agencia.getCodigo()));
+            pStatement.setString(2, agencia.getLocalizacao());
+
+            pStatement.execute();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+            throw new PersistenciaException("Não foi possível carregar o driver de conexão com a base de dados");
+        } catch (SQLException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+            throw new PersistenciaException("Erro ao enviar o comando para a base de dados");
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public void alterar(Agencia agencia) throws PersistenciaException {
+        String sql = String.format("UPDATE %s SET Localizacao=? WHERE NumeroAgencia = ?", nomeTabela);
+
+        Connection connection = null;
+        try {
+            connection = Conexao.getInstance().getConnection();
+            PreparedStatement pStatement = connection.prepareStatement(sql);
+
+            pStatement.setString(1, agencia.getLocalizacao());
+            pStatement.setString(2, Double.toString(agencia.getCodigo()));
+
+            pStatement.execute();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+            throw new PersistenciaException("Não foi possível conectar à base de dados!");
+        } catch (SQLException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+            throw new PersistenciaException("Não foi possível enviar o comando para a base de dados!");
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public void remover(Agencia agencia) throws PersistenciaException {
+        String sql = String.format("DELETE FROM %s WHERE NumeroAgencia = ?", nomeTabela);
+
+        Connection connection = null;
+        try {
+            connection = Conexao.getInstance().getConnection();
+
+            PreparedStatement pStatement = connection.prepareStatement(sql);
+            pStatement.setLong(1, agencia.getCodigo());
+
+            pStatement.execute();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+            throw new PersistenciaException("Não foi possível conectar à base de dados!");
+        } catch (SQLException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+            throw new PersistenciaException("Não foi possível enviar o comando para a base de dados!");
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public Agencia listarPorId(Agencia agencia) throws PersistenciaException {
+        String sql = String.format("SELECT * FROM %s WHERE NumeroAgencia = ?", nomeTabela);
+        Connection connection = null;
+        
+        try {
+            connection = Conexao.getInstance().getConnection();
+            PreparedStatement pStatement = connection.prepareStatement(sql);
+            
+            pStatement.setLong(1, agencia.getCodigo());
+            
+            ResultSet result = pStatement.executeQuery();
+            if (result.next()) {
+                agencia = montaObjeto(result);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return agencia;
+    }
+    
+    private Agencia montaObjeto(ResultSet result) throws SQLException{
+        return new Agencia(result.getInt("NUMEROAGENCIA"),
+                           result.getString("LOCALIZACAO"));
+    }
+}
